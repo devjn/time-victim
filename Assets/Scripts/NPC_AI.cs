fd666b1d45e3cedb217504bfbd81abb5b6bb9498
Alex <pyroman@the-ugc.com>
2016-07-26 09:36:02 +0300

    Updated the AI.

--- Assets/Scripts/NPC_AI.cs
+++ Assets/Scripts/NPC_AI.cs
@@ -13,49 +13,82 @@ public class NPC_AI : MonoBehaviour {
     private float c = 0;
     public float radius = 50;
 
-	// Use this for initialization
-	void Start () {
+    public Transform target;
+    private float minDistance = 1f;
+    private float range;
+
+    // Use this for initialization
+    void Start () {
         rbody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-        if (x == 0 && y == 1 && c > 50)
-        {
-            y = 0;
-            x = -1;
-            c = 0;
-        }
-        else if(x == -1 && y == 0 && c > 50)
-        {
-            x = 0;
-            y = -1;
-            c = 0;
-        }else if (x == 0 && y == -1 && c > 50)
-        {
-            x = 1;
-            y = 0;
-            c = 0;
-        }else if (x == 1 && y == 0 && c > 50)
-        {
-            x = 0;
-            y = 1;
-            c = 0;
-        }
-        Vector2 movement_vector = new Vector2(x, y);
-        if (movement_vector != Vector2.zero)
-        {
-            anim.SetBool("iswalking", true);
-            anim.SetFloat("input_x", movement_vector.x);
-            anim.SetFloat("input_y", movement_vector.y);
-        }
-        else
+
+    void Update()
+    {
+        if (Vector2.Distance(rbody.position, target.position) >= 6)
         {
             anim.SetBool("iswalking", false);
+            if (x == 0 && y == 1 && c > 50)
+            {
+                y = 0;
+                x = -1;
+                c = 0;
+            }
+            else if (x == -1 && y == 0 && c > 50)
+            {
+                x = 0;
+                y = -1;
+                c = 0;
+            }
+            else if (x == 0 && y == -1 && c > 50)
+            {
+                x = 1;
+                y = 0;
+                c = 0;
+            }
+            else if (x == 1 && y == 0 && c > 50)
+            {
+                x = 0;
+                y = 1;
+                c = 0;
+            }
+            Vector2 movement_vector = new Vector2(x, y);
+            if (movement_vector != Vector2.zero)
+            {
+                anim.SetBool("iswalking", true);
+                anim.SetFloat("input_x", movement_vector.x);
+                anim.SetFloat("input_y", movement_vector.y);
+            }
+            else
+            {
+                anim.SetBool("iswalking", false);
+            }
+
+            rbody.MovePosition(rbody.position + movement_vector * Time.deltaTime * speed);
+            c++;
+            return;
         }
 
-        rbody.MovePosition(rbody.position + movement_vector * Time.deltaTime * speed);
-        c++;
-	}
+        range = Vector2.Distance(transform.position, target.position);
+
+        if (range > minDistance)
+        {
+            Debug.Log(range);
+
+            Vector2 movement_vector = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime) - rbody.position;
+
+            if(movement_vector != Vector2.zero)
+            {
+                anim.SetBool("iswalking", true);
+                anim.SetFloat("input_x", movement_vector.x);
+                anim.SetFloat("input_y", movement_vector.y);
+            }
+            else
+            {
+                anim.SetBool("iswalking", false);
+            }
+
+            rbody.MovePosition(Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime));
+        }
+    }
 }
