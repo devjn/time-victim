0706c7eba630545f1cf037848426b21c8e607138
Ponomarev Mikhail <omipo016@edu.mamk.fi>
2016-07-27 10:32:51 +0300

    Health and Attack scripts

---
+++ Assets/Scripts/AttackScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Projectile behavior
+/// </summary>
+public class AttackScript : MonoBehaviour
+{
+    // 1 - Designer variables
+
+    /// <summary>
+    /// Damage inflicted
+    /// </summary>
+    public int damage = 20;
+
+    /// <summary>
+    /// Projectile damage player or enemies?
+    /// </summary>
+    public bool isEnemyAttack = false;
+
+    void Start()
+    {
+        // 2 - Limited time to live to avoid any leak
+        Destroy(gameObject, 20); // 20sec
+    }
+}
---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Handle hitpoints and damages
+/// </summary>
+public class HealthScript : MonoBehaviour
+{
+    /// <summary>
+    /// Total hitpoints
+    /// </summary>
+    public int hp = 100;
+
+    /// <summary>
+    /// Enemy or player?
+    /// </summary>
+    public bool isEnemy = true;
+
+    /// <summary>
+    /// Inflicts damage and check if the object should be destroyed
+    /// </summary>
+    /// <param name="damageCount"></param>
+    public void Damage(int damageCount)
+    {
+        hp -= damageCount;
+
+        if (hp <= 0)
+        {
+            // Dead!
+            GameManager.instance.GameOver();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        // Is this a shot?
+        AttackScript attack = otherCollider.gameObject.GetComponent<AttackScript>();
+        if (attack != null)
+        {
+            // Avoid friendly fire
+            if (attack.isEnemyAttack != isEnemy)
+            {
+                Damage(attack.damage);
+
+                // Destroy the shot
+                Destroy(attack.gameObject); // Remember to always target the game object, otherwise you will just remove the script
+            }
+        }
+    }
+}
