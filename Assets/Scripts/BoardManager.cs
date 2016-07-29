0b37474c66f2e3e5569650ee518b07de1f5460bc
Fayzullaev Jakhongir <Emperor117@rambler.ru>
2016-07-29 09:25:03 +0300

    Levels

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -27,7 +27,10 @@ public class BoardManager : MonoBehaviour
 
     public GameObject exit;
 	public GameObject[] floorTiles;
-	public GameObject[] wallTiles;
+    public GameObject[] floorTiles2;
+    public GameObject[] floorTiles3;
+
+    public GameObject[] wallTiles;
 	public GameObject[] foodTiles;
 	public GameObject[] enemyTiles;
 	public GameObject[] outerWallTiles;
@@ -49,13 +52,21 @@ public class BoardManager : MonoBehaviour
 		}
 	}
 
-	void BoardSetup ()
+	void BoardSetup (int level)
 	{
 		boardHolder = new GameObject ("Board").transform;
 
-		for (int x = -1; x < columns + 1; x++) {
+        GameObject[] floors;
+        if (level == 1)
+            floors = floorTiles;
+        else if (level == 2)
+            floors = floorTiles2;
+        else floors = floorTiles3;
+
+
+        for (int x = -1; x < columns + 1; x++) {
 			for (int y = -1; y < rows + 1; y++) {
-				GameObject toInstantiate = floorTiles [Random.Range (0, floorTiles.Length)];
+				GameObject toInstantiate = floors[Random.Range (0, floors.Length)];
 				if (x == -1 || x == columns || y == -1 || y == rows)
 					toInstantiate = outerWallTiles [Random.Range (0, outerWallTiles.Length)];
 
@@ -103,13 +114,13 @@ public class BoardManager : MonoBehaviour
 
 	public void SetupScene(int level)
 	{
-		BoardSetup ();
+		BoardSetup (level);
 		InitialiseList ();
 		LayoutObjectAtRandom (wallTiles, wallCount.minimum, wallCount.maximum);
 		LayoutObjectAtRandom (foodTiles, foodCount.minimum, foodCount.maximum);
         LayoutObjectAtRandom(trees, treeCount.minimum, treeCount.maximum);
         int enemyCount = (int)Mathf.Log (level, 2f) + 1;
-		LayoutObjectAtRandom (enemyTiles, enemyCount, enemyCount);
+		LayoutObjectAtRandom (enemyTiles, enemyCount, enemyCount + 2);
 		Instantiate (exit, new Vector3 (columns - 1, rows - 1, 0F), Quaternion.identity);
 	}
 
